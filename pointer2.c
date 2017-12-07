c225aef408990f025cc9d641351503f5e829dd81
Xintong (zncu) <xintongc@gmail.com>
2017-12-07 10:54:38 -0500

    Update pointer2.c

--- pointer2.c
+++ pointer2.c
@@ -50,3 +50,46 @@ int main() {
 
     return 0;
 }
+
+
+int main() {
+    int c = 79;
+    printf("location of c is %p \n",&c);
+    printf("value of c is %d \n", *&c);
+    //在变量前面加 & 取到变量的地址
+    //在地址前面加 * 取到值
+
+
+    //定义一个指针变量
+    int *pointer;
+    pointer = &c; //指针变量pointer指向了变量c
+    //pointer 中存的是 变量c， 而不是数字79
+
+    printf("the value of c is %d\n",*pointer);
+
+
+    int a = 0, b = 0, temp;
+    int *p1 = NULL, *p2 = NULL;
+
+    printf("input a:");
+    scanf("%d",&a);
+
+    printf("input b:");
+    scanf("%d",&b);
+
+    p1 = &a;
+    p2 = &b;
+
+    if(*p1 < * p2)
+    {
+        temp = *p1;
+        *p1 = *p2;
+        *p2 = temp;
+    }//交换的是值，*p1依然指向a，*p2指向b
+
+    printf("max is %d\n", *p1);
+    printf("min is %d", *p2);
+
+
+    return 0;
+}
